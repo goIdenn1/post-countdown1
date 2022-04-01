74b419d006061122d2c8727bc92d1cc4730060f7
Jav <jt6xnk@virginia.edu>
2022-03-31 20:31:37 -0400

    started completing the game and reorder some code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,18 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+        <button className="square" 
+        onClick={() => this.props.onClick()}>
+          {this.state.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null) //9 empty squares
+      };
+    }
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
     renderSquare(i) {
-      return <Square value={i} />;
+      return <Square 
+              value={this.state.squares[i]} 
+              onClick={() => this.handleClick(i)}/>;
     }
   
     render() {
